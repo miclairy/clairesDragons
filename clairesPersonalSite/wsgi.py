d688621098f89de1e10cc73936e8da3117e86d62
claire.barnaby <clb.miclairy@gmail.com>
2024-09-15 17:55:52 +0200

    dot env in wsgi

--- clairesPersonalSite/wsgi.py
+++ clairesPersonalSite/wsgi.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/clairesDragonApp')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clairesPersonalSite.settings')
 
