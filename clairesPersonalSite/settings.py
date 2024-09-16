9d90b418e5e859673c78c32fb96622186cacc356
claire.barnaby <clb.miclairy@gmail.com>
2024-09-17 01:11:59 +0200

    uninstall taiwind

--- clairesPersonalSite/settings.py
+++ clairesPersonalSite/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'clairesPersonalSite.urls'
