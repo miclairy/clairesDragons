de8e53e66e590dc0d9d37ce09df0b818e9ed7639
claire.barnaby <clb.miclairy@gmail.com>
2024-09-16 17:58:39 +0200

    crudely disable generate button while it is loading

--- clairesDragonApp/views.py
+++ clairesDragonApp/views.py
@@ -1,3 +1,4 @@
+import time
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -19,7 +20,7 @@ def create(request):
         form = DragonForm(request.POST)
         if (form.is_valid()):
             dragon = form.save(commit=True)
-           # url = 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-9pMbbb61J0DDJ6qYQu5oefM4/user-K6Ykb8YL2njruV5ShTW872d3/img-juzuEM4scEKEyDjA9wShwZZi.png?st=2024-09-16T13%3A36%3A33Z&se=2024-09-16T15%3A36%3A33Z&sp=r&sv=2024-08-04&sr=b&rscd=inline&rsct=image/png&skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-09-15T23%3A17%3A55Z&ske=2024-09-16T23%3A17%3A55Z&sks=b&skv=2024-08-04&sig=SQ4fMcr4bcsjYQkj0YpFpd1rNzvxbZ/aoQwls0Y/bMw%3D'
+            # url = 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-9pMbbb61J0DDJ6qYQu5oefM4/user-K6Ykb8YL2njruV5ShTW872d3/img-juzuEM4scEKEyDjA9wShwZZi.png?st=2024-09-16T13%3A36%3A33Z&se=2024-09-16T15%3A36%3A33Z&sp=r&sv=2024-08-04&sr=b&rscd=inline&rsct=image/png&skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-09-15T23%3A17%3A55Z&ske=2024-09-16T23%3A17%3A55Z&sks=b&skv=2024-08-04&sig=SQ4fMcr4bcsjYQkj0YpFpd1rNzvxbZ/aoQwls0Y/bMw%3D'
             url = generate(dragon)
             
     return render(request, 'clairesDragonApp/create.html', {'dragonForm': form, 'dragonUrl': url})
